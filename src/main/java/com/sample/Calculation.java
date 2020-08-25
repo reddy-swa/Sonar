71428e4526d3c3f273c3e647eb86b9b6141523ee
Jayavardhan Chowki <39634557+jayavardhan123@users.noreply.github.com>
2020-08-25 12:52:55 -0400

    commit

---
+++ src/main/java/com/sample/Calculation.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class Calculation {
+	
+	int a, b;
+	
+	public int addition(int a, int b) {
+		return a + b;
+	}
+
+}
---
+++ src/test/java/com/sample/testCalculation.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class testCalculation {
+	
+	Calculation obj = new Calculation();
+	
+	@Test
+	public void testAddition() {
+		assertEquals(4, obj.addition(2,2));
+	}
+
+}
